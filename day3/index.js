995f7f937fd49fa71ba4a407440c2608c2608f26
Eric Bishop <futureflyer.eric@gmail.com>
2020-12-07 15:00:47 -0700

    Day 3 done!

--- day3/index.js
+++ day3/index.js
@@ -1,5 +1,4 @@
-export const input = `
-........#..#.##.#..............
+const input = `........#..#.##.#..............
 ...#...............#.#.........
 ...#..#...#..##....#...........
 ...#.............#....#.....#..
@@ -321,12 +320,33 @@ export const input = `
 ...........#.#.............#...
 ...#..#.....#..##.#....#......#
 ..#...#..#...#......#..........
-....#..#....#.......#........#.
-`;
+....#..#....#.......#........#.`;
 
-let slopeX = 3;
-let slopeY = 1;
+const toboggans = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2]
+];
 
-let width = input.split("\n")[0].length;
+const trees = [];
 
-console.log(width);
+toboggans.forEach(toboggan => {
+  trees.push(0);
+  let x = 0
+  let y = 0;
+
+  let map = input.split("\n");
+
+  while (y < map.length) {
+    map[y][x % map[y].length] === "#" ? trees[trees.length - 1]++ : null;
+
+    x += toboggan[0];
+    y += toboggan[1];
+  }
+
+});
+
+console.log(trees[1]);
+console.log(trees.reduce((c, tree) => c * tree, 1));
