b4d0e91cb664fe8efa524352dbe68d7d5e463f3d
Eric Bishop <futureflyer.eric@gmail.com>
2020-12-08 20:28:47 -0700

    Day 6 part 2 done!

--- day6/index.js
+++ day6/index.js
@@ -5,6 +5,8 @@ let part1 = 0, part2 = 0;
 
 input.split("\n\n").forEach(val => { part1 += [...new Set(val.replace(/\n/g, "").split("").sort().join(""))].join("").length });
 
-// console.log(input.split("\n\n"));
+let groups = input.split("\n\n").map(val => val.split("\n").map(person => person.split("")));
+groups.forEach(person => part2 += person.reduce((result, curr) => curr.filter(currentItem => result.indexOf(currentItem) !== -1)).join("").length);
 
 console.log(part1);
+console.log(part2);
