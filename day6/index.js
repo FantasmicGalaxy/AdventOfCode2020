0a0b8837cf89eaffd5e8bb2ae9d64980ff6e1d2f
Eric Bishop <futureflyer.eric@gmail.com>
2020-12-08 20:01:19 -0700

    Day 6 part 1 is done

--- day6/index.js
+++ day6/index.js
@@ -1,2 +1,10 @@
 const fs = require("fs");
 const input = fs.readFileSync("day6/input.txt", "utf8");
+
+let part1 = 0, part2 = 0;
+
+input.split("\n\n").forEach(val => { part1 += [...new Set(val.replace(/\n/g, "").split("").sort().join(""))].join("").length });
+
+// console.log(input.split("\n\n"));
+
+console.log(part1);
